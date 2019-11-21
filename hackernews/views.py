181000e1415bf3e14ce0608c75a2834fc6236275
Kam Lotfull <kamlotfull@gmail.com>
2019-11-21 20:55:44 +0300

    Fixes possible limit, offset, order issues using `generics.ListAPIView`

--- hackernews/views.py
+++ hackernews/views.py
@@ -1,10 +1,27 @@
-from rest_framework import response, views
+import os
+
+from rest_framework import generics, filters, pagination
 
 from . import api
+from .models import Post
 
 
-class PostsView(views.APIView):
-    def get(self, request):
-        posts = api.posts.get(request.GET)
-        posts_serialized = api.serializers.PostSerializer(posts, many=True).data
-        return response.Response(posts_serialized)
+class PostsLimitOffsetPagination(pagination.LimitOffsetPagination):
+    default_limit = os.environ.get('POSTS_API_LIMIT_DEFAULT', 5)
+    max_limit = os.environ.get('POSTS_API_LIMIT_MAX', 100)
+
+
+class PostsView(generics.ListAPIView):
+    serializer_class = api.serializers.PostSerializer
+    pagination_class = PostsLimitOffsetPagination
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = '__all__'
+    ordering = ['created']
+
+    def get_queryset(self):
+        force = self.request.GET.get('force')
+        force = force is True or force == '1' or force == 'true' or force == 'True'
+        if force or not Post.objects.exists():
+            api.posts.scrap()
+
+        return Post.objects.all()
